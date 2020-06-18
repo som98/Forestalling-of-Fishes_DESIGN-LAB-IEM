25da2f377a33be952c2f5d2ae7ebcd7df185ef59
Somshubhra Roy <roysomshubhra98@gmail.com>
2020-06-18 20:21:38 +0530

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,40 +1,40 @@
 from mcc import *
 class water_sensor_1:
 
-    def __init__(self, id, waterlevel):
-        self.id = id
-        self.waterlevel=waterlevel
+    def __init__(self):
+        self.id=None
 
     def set_address(self, mac):
         self.mac=mac
 
     def get_address(self):
-        return self.mac_address
+        return self.mac
 
     def raise_alarm(self, id, waterlevel, threshold):
         filename = "raise_alarm_"+id
         alarm1 = open(filename, "w")
+        alarm =''
         if (waterlevel > threshold):
-            alarm1.write('1')
+            alarm='1'
         elif (waterlevel < 0):
-            alarm1.write("error")
+            alarm="error"
         elif (waterlevel <= threshold):
-            alarm1.write('0')
+            alarm ='0'
+        alarm1.write(alarm)
+        return alarm
 
 if __name__=='__main__':
-
     mcc_object = main_control_center()
-    id = input("Enter sensor id :")
+    num = int(input("Enter no. of sensors :"))
+    threshold = mcc_object.configure_threshold()
+    sens_obj = water_sensor_1()
+    for i in range (1,num+1):
+        water_level = input("Enter water level detected in sensor " + str(i) + " : ")
+        open("waterlevel_"+str(i) ,'w').write(water_level)
+        mac=input("Enter MAC address : ")
+        sens_obj.set_address(mac)
+        sens_obj.raise_alarm(str(i), int(water_level),threshold)
+        print("File written")
 
-    water_level = input("Enter water level detected in sensor "+id+" : ")
-
-    open("waterlevel_"+id ,'w').write(water_level)
-
-    threshold=mcc_object.configure_threshold()
-    mac=input("Enter MAC address : ")
-    sens_obj = water_sensor_1(id, int(water_level))
-    sens_obj.set_address(mac)
-    sens_obj.raise_alarm(id, int(water_level),threshold)
-    print("File written")
 
 
